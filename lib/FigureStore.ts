c4a92e5a2c254de00269fe5bba97f27b33154d3a
PromiSe#### <vicimpa@ya.ru>
2021-07-17 20:52:10 +0300

    Stream #1

---
+++ lib/FigureStore.ts
@@ -0,0 +1,65 @@
+import { Figure } from "./Figure";
+
+export const I = (
+  Figure
+    .make(
+      '    ',
+      '####',
+      '    ',
+      '    ',
+    )
+)
+
+export const O = (
+  Figure
+    .make(
+      '##',
+      '##'
+    )
+)
+
+export const T = (
+  Figure
+    .make(
+      '   ',
+      '###',
+      ' # '
+    )
+)
+
+export const J = (
+  Figure
+    .make(
+      ' # ',
+      ' # ',
+      '## '
+    )
+)
+
+export const L = (
+  Figure
+    .make(
+      ' # ',
+      ' # ',
+      ' ##'
+    )
+)
+
+export const S = (
+  Figure
+    .make(
+      ' ##',
+      '## ',
+      '   '
+    )
+)
+
+export const Z = (
+  Figure
+    .make(
+      '## ',
+      ' ##',
+      '   '
+    )
+)
+
