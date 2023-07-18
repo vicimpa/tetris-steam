80ac0efdf6b6765f6ae9b4d68fc1990feab4cdbc
PromiSe#### <vicimpa@ya.ru>
2023-07-19 02:27:46 +0700

    Fix generation seed

--- src/data/rand.ts
+++ src/data/rand.ts
@@ -3,7 +3,7 @@ import { randomSeed } from "lib/randomSeed";
 let seed = +location.hash.slice(1) | 0;
 
 if (!seed || isNaN(seed)) {
-  location.hash = `#${seed = (Math.random() * 100000000000 | 0)}`;
+  location.hash = `#${seed = Math.abs(Math.random() * 100000000000 | 0)}`;
 }
 
 export const rand = randomSeed(seed);
